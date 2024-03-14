867c243e3a032bb2b400827aa2312533ce7aa69c
chrisIyer <hellohotbox420@gmail.com>
2024-03-14 18:51:34 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import Login from './Login';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
