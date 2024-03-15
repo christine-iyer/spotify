b3bceecc4f1030a4c3ddba44bfa22928a34e830a
chrisIyer <hellohotbox420@gmail.com>
2024-03-15 16:25:08 -0400

    redirect not working.

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Container } from'react-bootstrap'
 
 const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=c2a4494357864a72a025e47f24f0355c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+ 
 
-
+ " https://accounts.spotify.com/authorize?client_id=c2a4494357864a72a025e47f24f0355c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 export default function Login() {
   return (
      <Container
