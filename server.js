4c75ba3dd77509e4a0f9b76b250d19ec1ed75dac
chrisIyer <hellohotbox420@gmail.com>
2024-03-19 17:04:39 -0400

    changing from axois to node.

--- server.js
+++ server.js
@@ -1,68 +1,21 @@
-require("dotenv").config()
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const lyricsFinder = require("lyrics-finder")
-const SpotifyWebApi = require("spotify-web-api-node")
-const PORT = process.env.PORT || 3001
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const express = require('express')
+const dotenv = require('dotenv');
 
+const port = 5000
 
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken,
-  })
+dotenv.config()
 
-  spotifyApi
-    .refreshAccessToken()
-    .then(data => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-app.post("/login", (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  })
+var app = express();
 
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
+app.get('/auth/login', (req, res) => {
+});
 
-app.get("/lyrics", async (req, res) => {
-  const lyrics =
-    (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
-  res.json({ lyrics })
-})
+app.get('/auth/callback', (req, res) => {
+});
 
-app.listen(PORT, () => {
-  console.log(`I am listening on ${PORT}. We In the Building.`)
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
 })
