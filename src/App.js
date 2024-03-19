c6e13ea2abad4421bdb1c01e379621c0a35229d0
chrisIyer <hellohotbox420@gmail.com>
2024-03-19 18:47:16 -0400

    so after doing a little (very little ) research, I decided not to go the axios route, because I read it was antiquated. (Not axios in general, just in this circumstance. But I've just twisted myself up. more. Not sure why this is even that important. Will reevaluate tomorrow. )

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import Login from "./Login"
-import Dashboard from "./Dashboard"
+// import "bootstrap/dist/css/bootstrap.min.css"
+// import Login from "./Login"
+// import Dashboard from "./Dashboard"
 
-const code = new URLSearchParams(window.location.search).get("code")
+// const code = new URLSearchParams(window.location.search).get("code")
 
-function App() {
-  return code ? <Dashboard code={code} /> : <Login />
+// function App() {
+//   return code ? <Dashboard code={code} /> : <Login />
+// }
+
+// export default App
+
+/**
+ * This is an example of a basic node.js script that performs
+ * the Client Credentials oAuth2 flow to authenticate against
+ * the Spotify Accounts.
+ *
+ * For more information, read
+ * https://developer.spotify.com/documentation/web-api/tutorials/client-credentials-flow
+ */
+
+const client_id = 'c2a4494357864a72a025e47f24f0355c'; 
+const client_secret = 'f10394ff7c314d50b9e4c6587ad9028f';
+
+async function getToken() {
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      'grant_type': 'client_credentials',
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64')),
+    },
+  });
+
+  return await response.json();
 }
 
-export default App
+async function getTrackInfo(access_token) {
+  const response = await fetch("https://api.spotify.com/v1/tracks/c2a4494357864a72a025e47f24f0355c", {
+    method: 'GET',
+    headers: { 'Authorization': 'Bearer ' + access_token },
+  });
+
+  return await response.json();
+}
+
+getToken().then(response => {
+  getTrackInfo(response.access_token).then(profile => {
+    console.log(profile)
+  })
+});
